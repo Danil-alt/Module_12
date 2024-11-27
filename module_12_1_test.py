b47da704349b68647a1152cb40ebbd4e5768c818
Danil-alt <kilindany24@gmail.com>
2024-11-27 05:45:35 +0500

    Add files via upload

---
+++ module_12_1_test.py
@@ -0,0 +1,30 @@
+import unittest
+from module_12_1 import Runner
+
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        runner = Runner('TestRunner')
+        for i in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+
+    def test_run(self):
+        runner = Runner('TestRunner')
+        for i in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge(self):
+        runner1 = Runner('Runner1')
+        runner2 = Runner('Runner2')
+        for i in range(10):
+            runner1.run()
+            runner2.walk()
+
+        self.assertNotEqual(runner1.distance, runner2.distance)
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
